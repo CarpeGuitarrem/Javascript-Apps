55404a3659d1f2f38ccd1baf2ef93593133b452e
AndyHauge <105992910+AndyHauge@users.noreply.github.com>
2022-09-05 16:23:35 -0400

    Update main.js

--- Business Card/main.js
+++ Business Card/main.js
@@ -2,7 +2,8 @@ const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 const width = canvas.width = 500;
 const height = canvas.height = 275;
-	
+
+//initial render of business card 
 ctx.fillStyle='white';
 ctx.strokeStyle='black';
 ctx.fillRect(0,0,width,height);
@@ -10,17 +11,35 @@ ctx.fillStyle='rgb(240,240,200)';
 ctx.fillRect(0,height*0.6,width,height);
 ctx.strokeRect(0,0,width,height);
 
-const myName = document.querySelector('input');
+//Referenced by draw()
+const myName = document.querySelector('#name');
 const myBusiness = document.querySelector('#business');
 const myTagline = document.querySelector('#tagline');
 
-/*myName.addEventListener('keyup', draw);
-myBusiness.addEventListener('keyup', draw);
-myTagline.addEventListener('keyup', draw);*/
-
+const btn = document.querySelector('#download');
 const menu = document.querySelector('.optionMenu');
 
 menu.addEventListener('keyup', draw);
+
+//function to convert render to file 
+download.addEventListener('click',() => { 
+	 let canvasImage = canvas.toDataURL('image/png');
+    
+    // this can be used to download any image from webpage to local disk
+    let xhr = new XMLHttpRequest();
+    xhr.responseType = 'blob';
+    xhr.onload = function () { //set function to create dummy download link to blob 
+        let a = document.createElement('a');
+        a.href = window.URL.createObjectURL(xhr.response);
+        a.download = 'business_card.png';
+        a.style.display = 'none';
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+      };
+      xhr.open('GET', canvasImage); // This is to download the canvas Image
+      xhr.send();
+});
 
 function draw(){
 	const name = myName.value;
